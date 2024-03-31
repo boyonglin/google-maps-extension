4b757f62363d87a2c0802f8c83acee73cc13b619
Clancy Lin <poerevan9@gmail.com>
2024-03-31 18:44:33 +0800

    Update preloadHTML url

--- Package/dist/background.js
+++ Package/dist/background.js
@@ -122,7 +122,7 @@ function addToFavoriteList(selectedText) {
 const preloadHTML = async () => {
   if (!(await chrome.offscreen.hasDocument())) {
     await chrome.offscreen.createDocument({
-      url: "popup.html",
+      url: "../popup.html",
       reasons: [chrome.offscreen.Reason.DISPLAY_MEDIA],
       justification: "Helps with faster load times of popup",
     });
