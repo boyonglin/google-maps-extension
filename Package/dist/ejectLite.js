7a4f8d8bcf00aed53947ced0c56afb99eb6a4e69
Clancy Lin <poerevan9@gmail.com>
2024-08-20 22:13:51 +0800

    remove annotation

--- Package/dist/ejectLite.js
+++ Package/dist/ejectLite.js
@@ -1,8 +1,7 @@
-// Wrapping in a function to not leak/modify variables if the script
 (function() {
     if ( typeof window.TME == "object"){
       if ( typeof window.TME.eject == "function" ){
         window.TME.eject();
       }
     }
-  })(); // <-- Invoke function. The return value is passed back to executeScript
+  })();
