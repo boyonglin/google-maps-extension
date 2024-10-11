3b8cad07732818cd5b9695f2bbd4bc12d78bc680
Clancy Lin <poerevan9@gmail.com>
2024-10-11 18:35:03 +0800

    clean code 1

--- Package/dist/background.js
+++ Package/dist/background.js
@@ -362,179 +362,50 @@ function meow() {
 // ExtensionPay
 importScripts("ExtPay.js")
 
-const trialPeriod = 7 * 24 * 60 * 60 * 1000 // 7 days
-const extpay = ExtPay("the-maps-express")
+const trialPeriod = 7 * 24 * 60 * 60 * 1000; // 7 days
+const extpay = ExtPay("the-maps-express");
 extpay.startBackground();
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+function handleExtensionPayment(user, sender) {
   const now = new Date();
 
-  if (request.action === "extPay") {
-    extpay.getUser().then(user => {
-      // First time user
-      if (!user.trialStartedAt) {
-        extpay.openTrialPage("7-day");
-        chrome.tabs.sendMessage(sender.tab.id, { action: "consoleQuote", stage: "first" });
-      }
+  // First time user
+  if (!user.trialStartedAt) {
+    extpay.openTrialPage("7-day");
+    chrome.tabs.sendMessage(sender.tab.id, { action: "consoleQuote", stage: "first" });
+  }
 
-      // Trial or Pay
-      else {
-        if (user.trialStartedAt && (now - user.trialStartedAt) < trialPeriod) {
-          extpay.openTrialPage();
-        } else {
-          extpay.openPaymentPage();
-          chrome.tabs.sendMessage(sender.tab.id, { action: "consoleQuote", stage: "payment" });
-        }
-      }
-    });
+  // Trial or Pay
+  else {
+    if (user.trialStartedAt && (now - user.trialStartedAt) < trialPeriod) {
+      extpay.openTrialPage();
+    } else {
+      extpay.openPaymentPage();
+      chrome.tabs.sendMessage(sender.tab.id, { action: "consoleQuote", stage: "payment" });
+    }
+  }
+}
+
+function checkPaymentStatus(user) {
+  const now = new Date();
+  const isPremium = user.paid;
+  const isFirst = !user.trialStartedAt && !isPremium;
+  const isTrial = user.trialStartedAt && (now - user.trialStartedAt) < trialPeriod && !isPremium;
+  const isFree = !isFirst && !isTrial && !isPremium;
+  const trialEnd = new Date(user.trialStartedAt).getTime() + trialPeriod;
+
+  return { isFirst, isTrial, isPremium, isFree, trialEnd };
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "extPay") {
+      extpay.getUser().then(user => handleExtensionPayment(user, sender));
   } else if (request.action === "restorePay") {
-    extpay.openLoginPage();
+      extpay.openLoginPage();
   } else if (request.action === "checkPay") {
-    extpay.getUser().then(user => {
-      const isPremium = user.paid;
-      const isFirst = !user.trialStartedAt && !isPremium;
-      const isTrial = user.trialStartedAt && (now - user.trialStartedAt) < trialPeriod && !isPremium;
-      const isFree = !isFirst && !isTrial && !isPremium;
-      const trialEnd = new Date(user.trialStartedAt).getTime() + trialPeriod;
-      sendResponse({ result: { isFirst, isTrial, isPremium, isFree, trialEnd } });
-    });
-    return true;
+      extpay.getUser().then(user => {
+          sendResponse({ result: checkPaymentStatus(user) });
+      });
+      return true;
   }
 });
-
-/********** Deprecated code **********/
-
-// let activeTabId = null;
-
-// // When the icon is clicked, inject or remove the iframe
-// chrome.action.onClicked.addListener(async (tab) => {
-//   if (tab.url && tab.url.startsWith("http")) {
-//     const iframeStatus = await getIframeStatus(tab.id);
-//     if (iframeStatus?.injected) {
-//       chrome.tabs.sendMessage(tab.id, { action: "removeIframe" });
-//     } else {
-//       tryInjectIframe(tab.id);
-//     }
-//   } else {
-//     console.error("Cannot execute extension on non-HTTP URL.");
-//   }
-// });
-
-// // Retry mechanism for injecting iframe (inject faster before the tab is fully loaded)
-// async function tryInjectIframe(tabId, retries = 10) {
-//   while (retries > 0) {
-//     try {
-//       await chrome.tabs.sendMessage(tabId, { action: "injectIframe" });
-//       return;
-//     } catch (error) {
-//       if (error.message.includes("Receiving end does not exist")) {
-//         await new Promise(resolve => setTimeout(resolve, 1000)); // Wait 1 second before retrying
-//         retries--;
-//       } else {
-//         throw error;
-//       }
-//     }
-//   }
-//   chrome.runtime.pendingAction = { tabId: tabId };
-// }
-
-// // Update iframe status
-// chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-//   if (sender.tab.id === activeTabId) {
-//     if (message.type === "iframeLoaded") {
-//       await setIframeStatus(activeTabId, true);
-//     } else if (message.type === "iframeRemoved") {
-//       await setIframeStatus(activeTabId, false);
-//     }
-//     updateIcon(activeTabId);
-//   }
-// });
-
-// chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-//   // When the tab is loaded, stop pending and inject the iframe
-//   if (chrome.runtime.pendingAction?.tabId === tabId && changeInfo.status === "complete") {
-//     chrome.tabs.sendMessage(tabId, { action: "injectIframe" });
-//     chrome.runtime.pendingAction = null;
-//   }
-
-//   // When the tab reloads, reset the iframe status
-//   const iframeStatus = await getIframeStatus(tab.id);
-//   if (changeInfo.status === "loading" && tab.active && iframeStatus?.injected) {
-//     await setIframeStatus(tabId, false);
-//     // updateIcon(tabId);
-//   }
-// });
-
-// // Handle active tab changes
-// chrome.tabs.onActivated.addListener(async (activeInfo) => {
-//   activeTabId = activeInfo.tabId;
-//   updateIcon(activeTabId);
-// });
-
-// // Handle active window changes
-// chrome.windows.onFocusChanged.addListener(async (windowId) => {
-//   // No window is focused
-//   if (windowId === chrome.windows.WINDOW_ID_NONE) {
-//     return;
-//   }
-
-//   // Get the active tab in the focused window
-//   const tabs = await chrome.tabs.query({ active: true, windowId: windowId });
-//   if (tabs.length > 0) {
-//     activeTabId = tabs[0].id;
-//     updateIcon(activeTabId);
-//   }
-// });
-
-// // Handle tab removal
-// chrome.tabs.onRemoved.addListener(async (tabId) => {
-//   await removeIframeStatus(tabId);
-// });
-
-// async function getIframeStatus(tabId) {
-//   const result = await chrome.storage.session.get(`iframeStatus_${tabId}`);
-//   return result[`iframeStatus_${tabId}`] || { injected: false };
-// }
-
-// async function setIframeStatus(tabId, injected) {
-//   await chrome.storage.session.set({ [`iframeStatus_${tabId}`]: { injected: injected } });
-// }
-
-// async function removeIframeStatus(tabId) {
-//   await chrome.storage.session.remove(`iframeStatus_${tabId}`);
-// }
-
-// // Function to update the icon based on iframe status
-// async function updateIcon(tabId) {
-//   const iframeStatus = await getIframeStatus(tabId);
-//   const path = iframeStatus?.injected ? {
-//     "16": "../images/icon-16.png",
-//     "32": "../images/icon-32.png",
-//     "48": "../images/icon-48.png",
-//     "128": "../images/icon-128.png"
-//   } : {
-//     "16": "../images/icon-opacity-16.png",
-//     "32": "../images/icon-opacity-32.png",
-//     "48": "../images/icon-opacity-48.png",
-//     "128": "../images/icon-opacity-128.png"
-//   };
-
-//   chrome.action.setIcon({ path });
-// }
-
-// // Update iframe X position when a window has been resized
-// chrome.windows.onBoundsChanged.addListener(async (window) => {
-//   const tabs = await chrome.tabs.query({ active: true, windowId: window.id });
-
-//   if (tabs.length > 0) {
-//     const activeTab = tabs[0];
-//     const url = activeTab.url;
-//     const tabId = activeTab.id;
-
-//     if (url && url.startsWith("http")) {
-//       chrome.tabs.sendMessage(tabId, { action: "adjustIframeX" });
-//     } else {
-//       return;
-//     }
-//   }
-// });
